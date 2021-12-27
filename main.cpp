18138fdc1e77ec400eca9647cf7af40a34fca98f
pwojton <piotr.wojton.01@gmail.com>
2021-12-27 18:13:48 +0100

    Dodanie szkieletu menu

--- main.cpp
+++ main.cpp
@@ -1,6 +1,44 @@
 #include <iostream>
+using namespace std;
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int wybor; //zmienna sluzaca do poruszania sie po menu
+
+	//menu
+	for (;;)
+	{
+		cout << "____BANK____" << endl;
+		cout << "1. Zaloguj sie" << endl;
+		cout << "2. Utworz konto" << endl;
+		cout << "3. Wyjdz" << endl << endl;
+		cout << "Wybierz operacje: ";
+		cin >> wybor;
+		cout << endl;
+
+		if (wybor == 1) //logowanie
+		{
+			system("CLS");
+		}
+		else if (wybor == 2)//tworzenie konta
+		{
+			system("CLS");
+		}
+		else if (wybor == 3)//wychodzenie
+		{
+			system("CLS");
+			cout << "Do widzenia" << endl;
+			break;
+		}
+		else
+		{
+			cout << "Podano bledna opcje" << endl;
+			system("CLS");
+		}
+
+	}
+
+
+
 }
