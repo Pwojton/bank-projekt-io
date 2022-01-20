042e442b65dfce9dd85b9b80c327bd6616920ef2
pwojton <piotr.wojton.01@gmail.com>
2022-01-20 23:04:31 +0100

    usuniecie testow jednostkowych

--- TestUnit/TestUnit.cpp
+++ TestUnit/TestUnit.cpp
@@ -12,9 +12,20 @@ namespace TestUnit
 		
 		TEST_METHOD(wykonajPrzelew)
 		{
-			//KontoFinanosowe Obiekt(12341, 100);
+			float saldoPierwsze = 1000;
+			float saldoDrugie = 0;
+			float kwota = 100;
 
+			kontoFinansowe Obiekt(123412, saldoPierwsze);
+			kontoFinansowe Obiekt2(331412, saldoDrugie);
 
+			Obiekt.wykonajPrzelew(Obiekt2, kwota);
+			
+			float wynik = saldoPierwsze - kwota;
+
+			Assert::AreEqual(kwota, Obiekt2.sprawdzSaldo());
+
+ 
 		}
 	};
 }
