fda17d415c50eed9fa5554791388485d6a24ae02
pwojton <piotr.wojton.01@gmail.com>
2022-01-20 21:55:46 +0100

    dodanie projektu z testami

---
+++ TestUnit/TestUnit.cpp
@@ -0,0 +1,20 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../kontoFinansowe.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TestUnit
+{
+	TEST_CLASS(KontoFinanosowe)
+	{
+	public:
+		
+		TEST_METHOD(wykonajPrzelew)
+		{
+			//KontoFinanosowe Obiekt(12341, 100);
+
+
+		}
+	};
+}
